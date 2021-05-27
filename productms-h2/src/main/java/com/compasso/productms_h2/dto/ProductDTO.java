392b5acc1adffa45d555e64dd7389af267e9fc8b
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2021-05-27 15:39:10 -0300

    Adicionado anotacoes do Lombok

--- productms-h2/src/main/java/com/compasso/productms_h2/dto/ProductDTO.java
+++ productms-h2/src/main/java/com/compasso/productms_h2/dto/ProductDTO.java
@@ -9,7 +9,11 @@ import javax.validation.constraints.Positive;
 import com.compasso.productms_h2.entities.Product;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter @NoArgsConstructor
 public class ProductDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -25,10 +29,7 @@ public class ProductDTO implements Serializable {
 	@NotNull
 	@Positive
 	Double price;
-
-	public ProductDTO() {
-	}
-
+		
 	public ProductDTO(Long id, String name, String description, Double price) {
 		this.id = id;
 		this.name = name;
@@ -42,36 +43,5 @@ public class ProductDTO implements Serializable {
 		this.description = entity.getDescription();
 		this.price = entity.getPrice();
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
-	}
+	
 }
--- productms-h2/src/main/java/com/compasso/productms_h2/entities/Product.java
+++ productms-h2/src/main/java/com/compasso/productms_h2/entities/Product.java
@@ -9,8 +9,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "tb_product")
+@Getter @Setter @NoArgsConstructor
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -23,45 +28,10 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 
-	public Product() {
-	}
-
 	public Product(Long id, String name, String description, Double price) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
-		this.price = price;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
-	public void setPrice(Double price) {
 		this.price = price;
 	}
 
